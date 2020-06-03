386b4a007e4a8404f8a8225b85a9e85ca5e05996
sarosh iqbal <goshi_86@hotmail.com>
2020-06-03 08:04:06 +0300

    hi

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,11 +8,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit{
   title = 'Skinet';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
   products: any[];
   constructor(private http: HttpClient){ }
   ngOnInit(): void{
    this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
-this.products = response.data;
+this.products = response;
+console.log(response);
    }, error => {
      console.log(error);
    });
